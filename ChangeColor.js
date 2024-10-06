608e6a1a7e41047865d448d6f75d796da8aa7d5a
Ankit gujre <144484381+ankitgujre01@users.noreply.github.com>
2024-10-07 00:08:42 +0530

    Add files via upload

---
+++ ChangeColor.js
@@ -0,0 +1,15 @@
+function colorBlack(){
+    document.body.style.background = "black";
+    // document.body.style.color = "pink";
+    document.querySelector('h1').style.color = "white";
+    
+}
+
+function colorGreen(){
+    document.body.style.background = "green";
+    document.querySelector('h1').style.color = "orange";
+}
+function Hover(){
+    document.body.style.color = "green";
+    // document.querySelector('h1').style.color = "orange";
+}
---
+++ ChangeImage.js
@@ -0,0 +1,9 @@
+function firstImage(){
+    let myImage = document.querySelector("#ImageId");
+    myImage.src = "https://wallpapers.com/images/hd/goku-cool-anime-6kbwj9794wpnsfr1.jpg";
+}
+
+function secondImage(){
+    let myImage = document.querySelector("#ImageId");
+    myImage.src = "https://wallpapers.com/images/hd/goku-cool-anime-6kbwj9794wpnsfr1.jpg";
+}
